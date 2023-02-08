3568a74f71b41df1a59349a24bee8879031be2f8
TREASUREM12 <treasuremtsweni@gmail.com>
2023-02-08 12:15:45 +0200

    Initial Commit

---
+++ Github software demo/Github_software_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_software_demoApp.swift
+//  Github software demo
+//
+//  Created by treasure on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct Github_software_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
